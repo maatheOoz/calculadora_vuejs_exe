bc8fdc6b6251233f1038dc7eae5a41520b121139
Matheus <106823213+maatheOoz@users.noreply.github.com>
2024-08-02 03:05:06 -0300

    commit de revisão

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -26,7 +26,7 @@ const calcularResultado = () => {
     <div class="container p-3 mb-2 bg-secondary text-white">
         <h1 class="mt-5 mb-3">Calculadora Aritmética</h1>
         <input class="form-control p-3 mb-2 bg-dark text-white" type="text" v-model="estado.primeiroNumero" @input="calcularResultado" />
-        <input type="text" class="form-control mt-2 p-3 mb-2 bg-dark text-white" v-model="estado.segundoNumero" @input="calcularResultado" />
+        <input class="form-control mt-2 p-3 mb-2 bg-dark text-white" v-model="estado.segundoNumero" @input="calcularResultado" />
 
         <select class="mt-3 mb-1 p-3 bg-dark text-white" v-model="estado.contaMatematica" @change="calcularResultado">
             <option value="soma">Soma</option>
